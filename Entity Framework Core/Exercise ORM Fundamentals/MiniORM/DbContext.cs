133fea2ecdbed7250e8ae56011df85d0884d436f
Vladimir Stanev <Stanev.vladi@gmail.com>
2023-11-04 19:04:34 +0200

    more
    :'DDDD

---
+++ Entity Framework Core/Exercise ORM Fundamentals/MiniORM/DbContext.cs
@@ -0,0 +1,17 @@
+﻿namespace MiniORM
+{
+    public class DbContext
+    {
+        public static Type[] AllowedSqlTypes =
+        {
+            typeof(string),
+            typeof(int),
+            typeof(uint),
+            typeof(long),
+            typeof(ulong),
+            typeof(decimal),
+            typeof(bool),
+            typeof(DateTime),
+        };
+    }
+}
---
+++ Entity Framework Core/Exercise ORM Fundamentals/MiniORM/DbSet.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+
+namespace MiniORM
+{
+    public class DbSet<TEntity>: ICollection<TEntity> where TEntity : class, new()
+    {
+        public IList<TEntity> Entities { get; set; }
+
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Add(TEntity item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(TEntity item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(TEntity[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<TEntity> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(TEntity item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
